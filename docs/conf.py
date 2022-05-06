ef924428ed4295a305759647e4a3e712a2396425
Björn Claremar <70746791+bclaremar@users.noreply.github.com>
2022-05-06 11:04:57 +0200

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -10,10 +10,10 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-# import os
-# import sys
+import os
+import sys
 # import sphinx_rtd_theme
-# sys.path.insert(0, os.path.abspath('.'))
+sys.path.insert(0, os.path.abspath('.'))
 
 
 # -- Project information -----------------------------------------------------
@@ -32,10 +32,13 @@ release = '0.1'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = ["sphinx_lesson",
-              'sphinx-prompt'
+              'sphinx-prompt', 
+              'sphinx.ext.autodoc'
 ]
 
-#source_suffix = ['.rst', '.md']
+source_suffix = ['.rst', '.md']
+
+master_doc = 'index'
 
 myst_enable_extensions = [
     "amsmath",
