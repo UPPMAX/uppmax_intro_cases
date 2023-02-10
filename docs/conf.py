d06f3d51578af711b2fe52b9dc0fce3ee574264c
Björn Claremar <70746791+bclaremar@users.noreply.github.com>
2023-02-10 17:23:45 +0100

    UU-logo fix

--- docs/conf.py
+++ docs/conf.py
@@ -73,11 +73,10 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # a list of builtin themes.
 #
 html_theme = 'sphinx_rtd_theme'
-html_logo = "img/UU_logo_color.svg"
-
-mermaid_output_format = "png"
-
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+html_logo = "_static/UU_logo_color.svg"
+
+mermaid_output_format = "png"
