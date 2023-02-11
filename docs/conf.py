f106322b6abe9c6df185e159ade276a850b6c0f6
Björn Claremar <70746791+bclaremar@users.noreply.github.com>
2023-02-11 14:59:02 +0100

    logo

--- docs/conf.py
+++ docs/conf.py
@@ -77,6 +77,10 @@ html_theme = 'sphinx_rtd_theme'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 #html_static_path = ['_static']
-html_logo = "_build/_static/UU_logo_color.svg"
-
+html_static_path = ['_static']
+html_logo = "img/UU_logo_color.svg"
+html_theme_options = {
+    'logo_only': True,
+    'display_version': False,
+}
 mermaid_output_format = "png"
